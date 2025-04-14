ac00c4de797559b95e4fb65154a070ef0e3a5628
krish vaghasiya <kvaghasiya705@gmail.com>
2025-04-14 18:23:13 +0530

    fix: Make header sticky by adding position styles

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -5,7 +5,7 @@ import FilterBar from "../../components/FilterBar";
 
 const Header = ({ onSearch }) => {
   return (
-    <header className="bg-cardGrey p-4 flex justify-between items-center font-pixel text-neon shadow-lg border-b-2 border-pixelBlue">
+    <header className="bg-cardGrey p-4 flex justify-between items-center font-pixel text-neon shadow-lg border-b-2 border-pixelBlue sticky top-0 left-0 z-[999999]">
       <Link
         to="/"
         className="w-[150px] hover:text-pixelYellow transition-all animate-pulse"
